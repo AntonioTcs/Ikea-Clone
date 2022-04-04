8c165e6238d2394755dd63768f2e65ed03d5782d
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-04-04 11:37:43 +0200

    Feat: Add sidebar hamburger and sidebar profile (reminder: resolve modules problem)

---
+++ script/sidebar-right-profile.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+    let profile = document.querySelector(".header-profile-link");
+    profile.addEventListener("click", () => {
+
+        function close() {
+            sidebar.style.opacity = "0";
+            sidebar.style.visibility = "hidden";
+            document.body.style.overflowY = "initial";
+        }
+
+        let sidebar = document.querySelector("aside#profile-sidebar");
+        let btnclose = sidebar.querySelector("#right-svg-container");
+        let closeSpace = sidebar.querySelector(".sidebar-opacity-zone");
+
+
+
+        sidebar.style.opacity = "1";
+        sidebar.style.visibility = "visible";
+        document.body.style.overflowY = "hidden";
+
+        btnclose.addEventListener("click", close);
+        closeSpace.addEventListener("click", close);
+    })
+}
