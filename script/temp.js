15b18336753dadbf063c886a3fa7dd2aec662ec4
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-04-06 00:03:06 +0200

    Feat: Finished JS interaction (reminder: link other page)

--- script/temp.js
+++ script/temp.js
@@ -60,4 +60,21 @@ window.onload = () => {
     closeBtn.addEventListener("click", close);
     closeSpace.addEventListener("click", close);
   });
+
+  let input = document.querySelectorAll("#inputContainer input");
+  let buttonForm = document.querySelector("#submitFormBtn");
+
+  buttonForm.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    for (let singleInput of input) {
+      console.log(
+        `${capitalizeFirstLetter(singleInput.id)}: ${singleInput.value}`
+      );
+    }
+  });
 };
